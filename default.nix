cd6c5812d80368773d5683a8e505d050eb2bb46f
Adam Joseph <F0B74D717CDE8412A3E0D4D5F29AC8080DA8E1E0>
2024-09-05 02:20:50 -0700

    move design notes into doc/

--- default.nix
+++ default.nix
@@ -292,29 +292,7 @@ let
               (target.${k} or (get-default-argument path v)))
           infusion);
 
-
-
-  ##############################################################################
-  # Representing missing attributes
-  ##############################################################################
-
-  #
-  # There are basically two ways to represent the "previous value" when infusing
-  # to a missing attribute:
-  #
-  # 1. Represent functions which can tolerate (or are sensitive to) a missing
-  #    previous value using { __functor = ... }.
-  #
-  #    - Downsides: invalidates eta-expansion, identity function is no longer
-  #      the left/right identity for function composition
-  #
-  # 2. Use a special "missing value" marker
-  #
-  #    - Downsides: although we can exploit function equality to create an
-  #      "unforgeable gensym", it isn't a `throw`.  So if it leaks outside of
-  #      infuse, ordinary functions might not force enough of the missing value
-  #      to trigger the error that they should experience.
-  #
+  # see doc/design-notes-on-missing-attributes.md
   get-default-argument =
     path: v:
     v.__default_argument or
---
+++ doc/design-notes-on-missing-attributes.md
@@ -0,0 +1,18 @@
+# Design notes on representation of "missing" target attributes:
+
+There are basically two ways to represent the "previous value" when infusing
+to a missing attribute:
+
+1. Represent functions which can tolerate (or are sensitive to) a missing
+   previous value using { __functor = ... }.
+
+   - Downsides: invalidates eta-expansion, identity function is no longer
+     the left/right identity for function composition
+
+2. Use a special "missing value" marker
+
+   - Downsides: although we can exploit function equality to create an
+     "unforgeable gensym", it isn't a `throw`.  So if it leaks outside of
+     infuse, ordinary functions might not force enough of the missing value
+     to trigger the error that they should experience.
+
